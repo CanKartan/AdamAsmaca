6c9536ebff70ee1618bdf3b3518eda4b17dfd6d8
Can Kartan <143809791+CanKartan@users.noreply.github.com>
2024-08-15 15:57:01 +0300

    kelimelerin gelmesi düzeltildi, alert eklendi

--- app.js
+++ app.js
@@ -1,28 +1,55 @@
 const word_el = document.getElementById("kelimeler");
+const correctLetters = [`a`, `r`, `m`,`u`,`t`]; 
+const wrongLetters = [];
 
-    function randomword() { 
-        let wordlist = document.getElementById("kelimeler");
+    // JSON içerisinden kelime seçme
+function randomword() { 
+    return new Promise((resolve, reject) => {
         let words = new XMLHttpRequest();
         words.open("GET", "kelimeler.json", true);
         words.onload = function() {
             if (this.status == 200) {
                 const Allwords = JSON.parse(this.responseText);
-                //! ekrana yazdırma kısmı düzeltilicek 
-                Allwords.forEach(word => {  
-                    wordlist.innerHTML = `
-                    <div class="letter">${word.kelimeler}</div>`;
-                });
-    
+                
                 const randomIndex = Math.floor(Math.random() * Allwords.length);
                 const randomWord = Allwords[randomIndex].kelimeler;
-                console.log(randomWord); 
+
+                resolve(randomWord);
+            } else {
+                reject("Kelimeler Yüklenemedi.");
             }
         };
-    
-        words.send(); 
-    }
+        
+        words.send();
+    });
+}
 
+function displayword(){
+    randomword().then(selectedword => {
+        word_el.innerHTML = `
+            ${selectedword.split('').map(letter => `
+                <div class="letter">
+                    ${correctLetters.includes(letter) ? letter : ` `}
+                </div>
+            `).join('')}
+        `;
+        
+        const i = word_el.textContent.replace(/\n/g, '').replace(/\s+/g, '');
+        if (i === selectedword){
+           const alert = document.createElement("div");
+           alert.className = "alert alert-success";
+           alert.innerHTML = `Kelimeyi Doğru Bildiniz.`
+            document.querySelectorAll(".mesaj")[0].appendChild(alert);
+            setTimeout(function(){
+                alert.remove();
+            },2000);
+        }
+        
+        // console.log(word_el.textContent.replace(/\n/g, '').replace(/\s+/g, ''));
+        
+    }).catch(error => {
+        console.error(error);
+    });
+}
 
-
-
-
+displayword();
