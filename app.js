a2d6d84da81703b0e5cec4df6fd18a32e7052721
Can Kartan <143809791+CanKartan@users.noreply.github.com>
2024-08-15 17:19:07 +0300

    harf girildiğinde adam oluşturulma işlemi tamamlandı

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const word_el = document.getElementById("kelimeler");
-const correctLetters = [`a`, `r`, `m`,`u`,`t`]; 
+const correctLetters = []; 
 const wrongLetters = [];
-
+let selectedword = '';  
+const wrongletters__El = document.getElementById("hatali__kelimeler");
+const items = document.querySelectorAll(".item");
 
 function randomword() { 
     return new Promise((resolve, reject) => {
@@ -23,24 +25,66 @@ function randomword() {
     });
 }
 
-function displayword(){
-    randomword().then(selectedword => {
-        word_el.innerHTML = `
-            ${selectedword.split('').map(letter => `
-                <div class="letter">
-                    ${correctLetters.includes(letter) ? letter : ` `}
-                </div>
-            `).join('')}
-        `;
-        const i = word_el.textContent.replace(/\n/g, '').replace(/\s+/g, '');
-        if (i === selectedword){
-            //Alert
-           Alert.alertmessage("Kelimeyi Doğru Bildiniz.","alert alert-success");
-        }
-        // console.log(word_el.textContent.replace(/\n/g, '').replace(/\s+/g, ''));
-    }).catch(error => {
-        console.error(error);
-    });
+function displayword() {
+    word_el.innerHTML = `
+        ${selectedword.split('').map(letter => `
+            <div class="letter">
+                ${correctLetters.includes(letter.toLowerCase()) ? letter : ` `}
+            </div>
+        `).join('')}
+    `;
+
+    const i = word_el.textContent.replace(/\n/g, '').replace(/\s+/g, '');
+    if (i.toLowerCase() === selectedword.toLowerCase()) {
+        Alert.alertmessage("Kelimeyi Doğru Bildiniz.","alert alert-success")
+    }
 }
 
-displayword();
+randomword().then(word => {
+    selectedword = word;  
+    displayword();  
+}).catch(error => {
+    console.error(error);
+});
+
+/* <h3>Hatalı Harfler</h3>
+<span>aaa</span> */
+
+function updatewrongletters(){
+    wrongletters__El.innerHTML = `
+        ${wrongLetters.length>0? `<h3>Hatalı Harfler</h3>`:``}
+        ${wrongLetters.map(letter => `<span>${letter}</span>`)}
+    `;
+    items.forEach((item,index) =>{
+        const errorcount = wrongLetters.length;
+        if(index<errorcount){
+            item.style.display = `block`;
+        }
+        else{
+            item.style.display = `none`;
+        }
+    })
+
+}
+
+window.addEventListener("keydown", function(e) {
+        const letter = e.key.toLowerCase();  
+
+        if (selectedword.toLowerCase().includes(letter)) {  
+            if (!correctLetters.includes(letter)) {
+                correctLetters.push(letter);
+                displayword(); 
+            }
+            else{
+                Alert.alertmessage("Bu Harf Eklendi.","alert alert-danger");
+            }
+        }
+        else{
+            if(!wrongLetters.includes(letter)){
+               wrongLetters.push(letter);
+                updatewrongletters();
+            }
+        }
+
+    
+});
